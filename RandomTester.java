2df6b86570c0d7c6d4a25edbb83742d0b5cddd99
Aimal <6318359@edu.vaniercollege.qc.ca>
2025-09-30 14:45:02 -0400

    Almost done, just need to answer the research questions

--- RandomTester.java
+++ RandomTester.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.ArrayList;
 
 /**
  * Write a description of class RandomTeste here.
@@ -10,6 +11,8 @@ public class RandomTester
 {
     // instance variables - replace the example below with your own
     private Random generator;
+    // collection of possible answers
+    private ArrayList<String> responses;
 
     /**
      * Constructor for objects of class RandomTeste
@@ -17,8 +20,52 @@ public class RandomTester
     public RandomTester()
     {
         generator = new Random();
+        responses = new ArrayList<String>();
+    }
+    
+    public void addResponse(String response)
+    {
+        responses.add(response);
+    }
+    
+    public int returnValidRandomNumber(int a, int b)
+    {
+        int max;
+        int min;
+        if(a > b)
+        {
+            max = a;
+            min = b;
+        }else{
+            max = b;
+            min = a;
+        }
+        int validNumber = generator.nextInt(min,max);
+        if(validNumber == 0)
+        {
+            validNumber++;
+            return validNumber;
+        }else{
+            return validNumber;
+        }
+    }
+    
+    public void getResponse()
+    {
+        int selectedIndex = generator.nextInt(responses.size());
+        System.out.println(responses.get(selectedIndex));
     }
 
+    public int throwDice()
+    {
+        int diceFace = generator.nextInt(6);
+        if(diceFace == 0){
+            return (diceFace + 1);
+        }else{
+            return diceFace;
+        }
+    }
+    
         /**
      * An example of a method - replace this comment with your own
      *
